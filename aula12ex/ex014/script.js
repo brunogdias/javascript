8a0bfba3fb6d61aaae255cfe7f6d26fc538620d7
brunogdias <alikasurb@hotmail.com>
2022-02-05 11:30:35 -0300

    .
    .

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,7 +1,7 @@
 function rodar() {
     var msg = document.getElementById('mensagem')
-    var img = docume.getElementById('imagem')
+    var img = document.getElementById('imagem')
     var agora = new Date()
-    var hora = agora.getHours
-    msg.innerHTML(`Agora são ${agora} horas.`)
+    var hora = agora.getHours()
+    msg.innerHTML =`Agora são ${hora} horas.`
 }
