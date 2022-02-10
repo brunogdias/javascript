30b46993e48779299e3ef09a81d85d7db41b93d0
brunogdias <alikasurb@hotmail.com>
2022-02-10 17:18:48 -0300

    .
    .

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,13 +1,13 @@
-// Um array é uma variável composta que tem vários elementos. Cada elemento é composto por seu valor e por uma chave (key) de identificação.
+// Um array (vetor) é uma variável composta que tem vários elementos. Cada elemento é composto por seu valor e por uma chave (key) de identificação.
 
 let num = [7,3,1,12,9]
-num.sort() // organizar os valores em ordem crescente considerando a primera casa
-num.push(34) // acrescenta o valor solicitado após o último valor
-console.log(num) // mostra/lê os vetores de num entre colchetes
+num.sort() // organizar os valores em ordem crescente considerando a primera casa decimal. É possível ordenar por ordem crescente utilizando: num.sort(function(a, b){return a-b})
+num.push(34) // acrescenta o valor entre parenteses após o último valor
+console.log(num) // mostra/lê os vetores da variavel num entre colchetes, considerando as modificações acima.
 console.log(`O vetor tem ${num.length} posições`) // mostra o total de posições de um vetor
-console.log(`O primeiro valor do vetor é ${num[0]}`) //mostra o valor da posição 0
+console.log(`O primeiro valor do vetor é ${num[0]}`) //mostra o valor do índice 0
 
-/* No exemplo acima num é o vetor ou array  
+/* No exemplo acima num é o vetor / array  
 O colchete [ ] chama-se elemento
 O que está dentro dos colchetes chama-se conteúdo/valor
-E as posições que o conteúdo/valor ocupa se chama índice/chave iniciando sempre pelo 0 No exemplo acima temo os índices  0 1 2 3 4 */
+E as posições que o conteúdo/valor ocupa se chama índice/chave iniciando sempre pelo 0. No exemplo acima a variavel num tem os índices  0 1 2 3 4 5 */
--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,17 +1,16 @@
-let val = [5, 3, 7, 4, 1, 9] 
-val.sort() // organizar os valores em ordem crescente considerando a primera casa decimal. Fica: [1, 3, 4, 5, 7, 9]. Em outras palavras o método sort() classifica os valores como strings em ordem alfabética e crescente. 
-// Mas é possível ordenar também valores numéricos utilizando: val.sort(function(a, b){return a-b})
+let val = [25, 3, 7, 4, 13, 9] 
+val.sort(function(a, b){return a-b}) // ordenar por ordem crescente
 
 console.log(val)
 
-console.log(val[0])
+console.log(val[0]) // mostra o valor no índice 0 que é 3
 console.log(val[1])
 console.log(val[2])
 console.log(val[3])
 console.log(val[4])
 console.log(val[5])
 
-for(let pos=0; pos < val.length; pos++) { // leia-se: enquanto 'pos' for menor que o comprimento do vetor (val.length), ele irá adicionar 'pos' a cada looping/repetição
+for(let pos=0; pos < val.length; pos++) { // leia-se: enquanto 'pos' for menor que o comprimento do vetor (val.length), ele irá adicionar 1 'pos' a cada looping/repetição
     console.log(`A posição ${pos} tem o valor ${val[pos]}`)
 }
 
@@ -24,24 +23,25 @@ for(let pos=0; pos < val.length; pos++) { // leia-se: enquanto 'pos' for menor q
 
 A estrutura a seguir simplifica a estrutura de cima: */
 
-for( let pos in val) {   
-    console.log(val[pos])
+let g = 0
+for( let g in val) {   
+    console.log(val[g])
 }
-// leia-se: para cada posição (pos) em val (vetores) será mostrado val[pos]
+// leia-se: para cada posição (g) em val (vetores) será mostrado val[g]
 
 
 // Há também a função .indexOf() que retonar/informa a chave/o índice em que está o valor que é colocado entre parênteses
 console.log(val.indexOf(7)) // resposta será indíce 4, pois o valor 7 está no índice 4, lembrando que foi utilizada a função val.sort() que reorganizou os valores
-console.log(val.indexOf(2)) // A resposta dará -1 o que significa para o js que o valor ñ foi encontrado
+console.log(val.indexOf(2)) // A resposta dará -1 o que significa para o js que o valor ñ foi encontrado no vetor/ na array
 
 let pos = val.indexOf(5)
 console.log(`O valor 8 está na posição ${pos}`)
 
 
 //Para não mostrar -1 quando o valor ñ é encontrado, pode-se construir a seguinte estrutura:
-let pos2 = val.indexOf(2)
-if (pos2 == -1) {
+let h = val.indexOf(2)
+if (h == -1) {    
     console.log('O valor não foi encontrado!')
 } else {
-    console.log(`O valor está na posição ${pos2}`)
+    console.log(`O valor está na posição ${h}`)
 }
